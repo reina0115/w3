ebfe075cd89ff3520e86cf7df2874e8d740236bd
reina0115 <131437146+reina0115@users.noreply.github.com>
2023-09-21 11:57:56 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,8 +17,20 @@ myButton.onclick = function() {
 // Image switcher code
 
 let myImage = document.querySelector('img');
-
+/*
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   console.log("画像をクリックしました");
 }
+*/
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
