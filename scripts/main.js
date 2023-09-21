6db1f473fbcb358f3112757534dcec76795dfafc
reina0115 <131437146+reina0115@users.noreply.github.com>
2023-09-21 12:12:56 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -27,7 +27,7 @@ myImage.onclick = function() {
 
 
 myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
+  let mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
     myImage.setAttribute("src", "images/firefox2.png");
   } else {
